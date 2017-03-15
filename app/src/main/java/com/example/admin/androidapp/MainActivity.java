abcd9b7715af2217205210d361e4c223364df68e
Damian <damian.zmuda22@gmail.com>
2017-03-15 01:17:15 +0100

    add a lot of elements

--- app/src/main/java/com/example/admin/androidapp/MainActivity.java
+++ app/src/main/java/com/example/admin/androidapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.androidapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendText(View view) {
+        EditText nameEditText = ((EditText) findViewById(R.id.nameEditText));
+        if (!nameEditText.getText().toString().isEmpty() && nameEditText.isEnabled()) {
+            ((TextView) findViewById(R.id.outputTextView)).setText(nameEditText.getText());
+            nameEditText.setEnabled(false);
+        } else {
+            nameEditText.setError(getResources().getString(R.string.emptyField));
+        }
+
+        if (!nameEditText.isEnabled()) {
+
+        }
     }
 }
